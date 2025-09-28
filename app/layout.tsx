ecdba33cd1e951eebbcbcc831d6d8c4a791a425e
marcolinofael <marcolinofael@gmail.com>
2025-09-28 11:41:36 -0300

    add matadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,33 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Rafa Consultoria",
-  description: "Consultoria especializada em soluções digitais",
+  description: "Consultoria especializada em soluções digitais e Personalizados",
+  keywords: ["consultoria", "tecnologia", "soluções digitais", "desenvolvimento web", "inovação"],
+  authors: [{ name: "Rafael Marcolino" }],
+  openGraph: {
+    title: "Rafa Consultoria",
+    description: "Consultoria especializada em soluções digitais e Personalizados",
+    url: "https://rafaconsultoria.site",
+    siteName: "Rafa Consultoria",
+    images: [
+      {
+        url: "/img/Logo.png",
+        width: 800,
+        height: 600,
+      },
+    ],
+    locale: "pt_BR",
+    type: "website",
+  },
+  icons: {
+    icon: "/img/Logo.png",
+    shortcut: "/img/Logo.png",
+    apple: "/img/Logo.png",
+  },
+  robots: {
+    index: true,
+    follow: true,
+  },
 };
 
 export default function RootLayout({
