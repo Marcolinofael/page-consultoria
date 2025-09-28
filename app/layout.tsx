be0210ae048bae50300e438b12d052773b5dd3ac
marcolinofael <marcolinofael@gmail.com>
2025-09-28 11:56:07 -0300

    add matadata2

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,25 +17,35 @@ export const metadata: Metadata = {
   description: "Consultoria especializada em soluções digitais e Personalizados",
   keywords: ["consultoria", "tecnologia", "soluções digitais", "desenvolvimento web", "inovação"],
   authors: [{ name: "Rafael Marcolino" }],
+  metadataBase: new URL('https://rafaconsultoria.site'),
   openGraph: {
     title: "Rafa Consultoria",
     description: "Consultoria especializada em soluções digitais e Personalizados",
-    url: "https://rafaconsultoria.site",
+    url: 'https://rafaconsultoria.site',
     siteName: "Rafa Consultoria",
-    images: [
-      {
-        url: "/img/Logo.png",
-        width: 800,
-        height: 600,
-      },
-    ],
+    images: {
+      url: '/img/Logo.png',
+      width: 800,
+      height: 600,
+      alt: 'Rafa Consultoria Logo',
+    },
     locale: "pt_BR",
     type: "website",
   },
+  twitter: {
+    card: 'summary_large_image',
+    title: "Rafa Consultoria",
+    description: "Consultoria especializada em soluções digitais e Personalizados",
+    images: ['/img/Logo.png'],
+  },
   icons: {
-    icon: "/img/Logo.png",
-    shortcut: "/img/Logo.png",
-    apple: "/img/Logo.png",
+    icon: '/img/Logo.png',
+    shortcut: '/img/Logo.png',
+    apple: '/img/Logo.png',
+    other: {
+      rel: 'apple-touch-icon',
+      url: '/img/Logo.png',
+    },
   },
   robots: {
     index: true,
